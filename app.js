ac0f2f75012134378f35b7b217fea9b29dd36c42
harisqasi <harisqasim45@gmail.com>
2024-03-24 03:16:44 +0500

    ch-1 ch-2 ch-3

---
+++ app.js
@@ -0,0 +1,36 @@
+//Topic 1
+//alert("haris");
+
+//Topic 2
+//let userName="haris";
+//console.log(userName);
+
+//Topic 3
+//var a= "99";
+//var num = Number(a);
+//console.log(num +11);
+
+//ParseInt 1St Example
+//var a="99 77 55";
+//var num=parseInt(a);
+//console.log(num);
+
+//parseInt 2nd Example without Decimal wali value return karta hai 
+ //var a= "10.99";
+// var num = parseInt(a);
+// console.log(num);
+
+//parseFloat
+var a= "10.99";
+ var num = parseFloat(a);
+ console.log(num);
+
+//Is Finite
+ //var a= 10;
+ //var num =Number.isFinite(a);
+ //console.log(num);
+
+ //Is Integer   
+ var a= 10;
+ var num =Number.isInteger(a);
+console.log(num);
